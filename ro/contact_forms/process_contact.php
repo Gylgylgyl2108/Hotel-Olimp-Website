7b74d11dac78b66dfbd2bf0a0a31fef1bb6a1a1a
Titi923 <virgil.cenan@gmail.com>
2024-02-15 11:21:43 +0200

    test contact form

--- ro/contact_forms/process_contact.php
+++ ro/contact_forms/process_contact.php
@@ -8,50 +8,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST["message"]);
 
     // Validate input (you can add more validation as needed)
-    if (empty($name) || empty($email) || empty($message)) {
-        echo "Please fill in all fields.";
+    // Set recipient email address
+    $to = "petrisor.buciutaa@gmail.com";
+
+    // Set subject
+    $subject = "Contact from Hotel Website by $name";
+
+    // Build text and HTML email parts
+    $text_message = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $html_message = <<<EOT
+    <p style="font-size: 20px;"><b>Nume:</b> $name</p>
+    <p style="font-size: 20px;"><b>Prenume:</b> $name</p>
+    <p style="font-size: 20px;"><b>Email:</b> $email</p>
+    <p style="font-size: 20px;"><b>Telefon :</b> $email</p>
+    <p style="font-size: 20px;"><b>Mesajul:</b> $message</p>
+    EOT;
+
+    // Set additional headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: multipart/alternative; boundary=\"boundary123\"";
+
+    // Build the email body with both text and HTML parts
+    $email_body = "--boundary123\r\n";
+    $email_body .= "Content-Type: text/plain; charset=\"UTF-8\"\r\n";
+    $email_body .= "Content-Transfer-Encoding: 7bit\r\n\n";
+    $email_body .= $text_message . "\r\n\n";
+    $email_body .= "--boundary123\r\n";
+    $email_body .= "Content-Type: text/html; charset=\"UTF-8\"\r\n";
+    $email_body .= "Content-Transfer-Encoding: 7bit\r\n\n";
+    $email_body .= $html_message . "\r\n\n";
+    $email_body .= "--boundary123--";
+
+    // Send email
+    $success = mail($to, $subject, $email_body, $headers);
+
+    // Check if mail was sent successfully
+    if ($success) {
+        header("Location: ../confirm_email.php");
     } else {
-        // Set recipient email address
-        $to = "petrisor.buciutaa@gmail.com";
-
-        // Set subject
-        $subject = "Contact Form Submission from $name";
-
-        // Build text and HTML email parts
-        $text_message = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-        $html_message = <<<EOT
-        <p><b>Name:</b> $name</p>
-        <p><b>Email:</b> $email</p>
-        <p><b>Message:</b> $message</p>
-        EOT;
-
-        // Set additional headers
-        $headers = "From: $email\r\n";
-        $headers .= "Reply-To: $email\r\n";
-        $headers .= "X-Mailer: PHP/" . phpversion();
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-type: multipart/alternative; boundary=\"boundary123\"";
-
-        // Build the email body with both text and HTML parts
-        $email_body = "--boundary123\r\n";
-        $email_body .= "Content-Type: text/plain; charset=\"UTF-8\"\r\n";
-        $email_body .= "Content-Transfer-Encoding: 7bit\r\n\n";
-        $email_body .= $text_message . "\r\n\n";
-        $email_body .= "--boundary123\r\n";
-        $email_body .= "Content-Type: text/html; charset=\"UTF-8\"\r\n";
-        $email_body .= "Content-Transfer-Encoding: 7bit\r\n\n";
-        $email_body .= $html_message . "\r\n\n";
-        $email_body .= "--boundary123--";
-
-        // Send email
-        $success = mail($to, $subject, $email_body, $headers);
-
-        // Check if mail was sent successfully
-        if ($success) {
-            header("Location: ../confirm_email.php");
-        } else {
-            echo "Oops! Something went wrong, and we couldn't send your message.";
-        }
+        echo "<h1 style='font-size: 50px color=red'>Oops! Something went wrong, and we couldn't send your message.</h1>";
     }
 }
 ?>
