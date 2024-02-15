b085e94383ca8826c8021222d175b039d8643050
Titi923 <virgil.cenan@gmail.com>
2024-02-15 13:47:42 +0200

    preturi

--- ro/preturi.php
+++ ro/preturi.php
@@ -1,8 +1,8 @@
 <?php
-$camera_single = 123;
-$camera_double_deluxe = 222;
-$camera_triple = 333;
-$camera_double_simple = 444;
-$camera_twin = 555;
-$camera_four_guests = 666;
+$camera_single = 160;
+$camera_double_deluxe = 200;
+$camera_triple = 260;
+$camera_double_simple = 180;
+$camera_twin = 170;
+$camera_four_guests = 320;
 ?>
