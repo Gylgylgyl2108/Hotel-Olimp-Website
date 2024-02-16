ae6a6196dbca8851ec1e242f35f40c2f52c193fb
Titi923 <virgil.cenan@gmail.com>
2024-02-16 13:26:37 +0200

    test

--- ro/terms_and_conditions.php
+++ ro/terms_and_conditions.php
@@ -33,12 +33,12 @@
                         Suntem conștienți că prelucrarea datelor cu caracter personal este un subiect extrem de
                         important, de aceea, această procedură descrie în detaliu fiecare flux din interiorul organizației, în care
                         sunt procesate date cu caracter personal ale clienților, partenerilor, potențialilor clienți sau ale
-                        vizitatorilor platformei.
+                        vizitatorilor platformei.<br></br>
                         VICEN SRL depune eforturi majore pentru a păstra în siguranță informațiile și preferințele
                         dumneavoastră. Înțelegem cât de important este să vă simțiți în siguranță atunci când vă aflați în mediul
                         online, de aceea vă asigurăm de confidențialitatea datelor dvs, având în vedere că în acest moment vă
                         aflați pe o conexiune criptată (SSL), prin care informațiile transmise sunt criptate și sunt procesate de noi
-                        doar cu acordul dumneavoastră.
+                        doar cu acordul dumneavoastră.<br></br>
                         Rețineți că în orice moment, pe parcursul colaborării noastre, vă puteți exprima opțiunea cu
                         privire la procesarea datelor dumneavoastră cu caracter personal. Suntem conștienți de faptul că
                         Regulamentul General privind Protecția Datelor (GDPR) oferă un cadru legal modernizat de
@@ -48,23 +48,23 @@
                         luăm pentru a asigura transparența și proporționalitatea față de dumneavoastră este aceea de a elimina
                         colectarea inutilă a datelor cu caracter personal și de a minimiza datele pe care le colectăm. De aceea,
                         solicităm doar informațiile de care avem nevoie și le stocăm pentru a îmbunătăți experiența
-                        dumneavoastră.
-                        Vă rugăm să citiți cu atenție termenii și condițiile înainte de a utiliza acest site.
+                        dumneavoastră.<br></br>
+                        Vă rugăm să citiți cu atenție termenii și condițiile înainte de a utiliza acest site.<br></br>
                          TERMENI ȘI CONDIȚII DE UTILIZARE
                         Site-ul https://www.hotel-olimp.ro/ro/ este proprietatea VICEN SRL, CUI RO16144568, Nr.
-                        Reg. Com. J12/546/2004, denumită în continuare VICEN SRL.
+                        Reg. Com. J12/546/2004, denumită în continuare VICEN SRL.<br></br>
                         Accesarea, vizitarea, folosirea sau cumpărarea produselor/serviciilor/pachetelor prezentate pe site-ul
-                        https://www.hotel-olimp.ro/ro/ implică acceptarea termenilor și condițiilor enumerate mai jos.
+                        https://www.hotel-olimp.ro/ro/ implică acceptarea termenilor și condițiilor enumerate mai jos.<br></br>
                         VICEN SRL își rezervă dreptul de a refuza colaborarea cu clienții care manifestă comportament și limbaj
-                        neadecvat (agresiv, licențios etc.) sau care au în istoricul colaborării debite.
+                        neadecvat (agresiv, licențios etc.) sau care au în istoricul colaborării debite.<br></br>
                         VICEN SRL își rezervă dreptul de a nu onora livrarea unor produse sau servicii a căror preț este greșit și
                         nu se încadrează în grila de prețuri a companiei. În acest caz, clientul va fi contactat și va avea opțiunea
-                        de a confirma comanda la prețul corect sau de a o refuza.
+                        de a confirma comanda la prețul corect sau de a o refuza.<br></br>
                         VICEN SRL își rezervă dreptul de a face modificări și actualizări ale acestor termeni și condiții, precum și
                         ale ofertei, fără o notificare prealabilă și fără precizarea motivelor. De asemenea, VICEN SRL își rezervă
                         dreptul de a corecta eventuale omisiuni sau erori în afișare care pot apărea în urma unor greșeli de
                         dactilografiere, lipsă de acuratețe sau erori ale produselor software, fără a anunța în prealabil. Aceste
-                        erori nu obligă VICEN SRL la nicio acțiune.
+                        erori nu obligă VICEN SRL la nicio acțiune.<br></br>
                          DREPTURI DE AUTOR
 
                         Conținutul și designul site-ului https://www.hotel-olimp.ro/ro/ sunt proprietatea VICEN SRL și
@@ -79,7 +79,7 @@
                         își asumă răspunderea, în nicio situație, pentru nicio daună pricinuită direct sau indirect, suferită ca
                         urmare a utilizării sau întreruperii utilizării ori a lipsei de regularitate a informațiilor și serviciilor
                         furnizate pe acest site. VICEN SRL nu garantează acuratețea, corectitudinea sau caracterul actual al
-                        informațiilor furnizate.
+                        informațiilor furnizate.<br></br>
                          COMENZI SI CERERI PENTRU OFERTE DE PRET
                         Comenzile sau cererile pentru oferte de preț pot fi făcute doar utilizând formularul de contact
                         de pe acest site. Utilizatorul are obligația de a furniza date corecte și cât mai concrete cu privire la
@@ -87,74 +87,74 @@
                         furnizarea de către utilizator a unor informații incorecte sau incomplete. În cazul în care VICEN SRL
                         consideră o cerere sau o comandă ca fiind incompletă sau incorectă, poate cere detalii suplimentare
                         utilizatorului folosind adresa de e-mail sau numărul de telefon de contact furnizat de acesta sau poate
-                        șterge cererea fără nicio notificare a utilizatorului.
+                        șterge cererea fără nicio notificare a utilizatorului.<br></br>
                          MODUL DE SOLUTIONARE AL LITIGIILOR
                         Soluționarea litigiilor se poate face pe cale amiabilă sau prin intermediul instanțelor judecătorești
                         competente sau prin intermediul unor mediatori. Comisia Europeană a implementat o platforma de
-                        solutionare online a litigiilor disponibila aici.
+                        solutionare online a litigiilor disponibila aici.<br></br>
                          NOTIFICARI SI ANUNTURI
                         VICEN SRL își rezervă dreptul de a trimite notificări privind serviciile oferite, precum și anunțuri
-                        din partea unor terți, atunci când acestea pot fi în folosul utilizatorilor.
+                        din partea unor terți, atunci când acestea pot fi în folosul utilizatorilor.<br></br>
                          DISPOZITII FINALE
                         Toți utilizatorii acestui site fac subiectul acestor termeni și condiții, astfel încât accesarea
                         acestuia presupune acceptarea și conformarea la termenii și condițiile prevăzute în acest document.
                         Scopul colectării datelor este furnizarea serviciilor conform ofertei curente. Orice persoană are dreptul
                         de a se opune, în mod gratuit și fără nicio justificare, la prelucrarea datelor sale personale în scopuri de
                         marketing direct. Conform legislației din România, minorii nu pot încheia contracte comerciale și nici
-                        datele lor personale nu sunt prelucrate fără acordul părinților.
+                        datele lor personale nu sunt prelucrate fără acordul părinților.<br></br>
                         Neacceptarea acestei convenții ori a oricărei prevederi din aceasta atrage obligația persoanei
                         respective de a înceta de îndată accesarea sitului ori utilizarea în orice mod a serviciului, iar accesarea
-                        site-ului și utilizarea acestuia se vor face pe propriul risc și în conformitate cu legile în vigoare.
+                        site-ului și utilizarea acestuia se vor face pe propriul risc și în conformitate cu legile în vigoare.<br></br>
 
                         Politica de Confidențialitate a Hotelului
                         Ultima actualizare: Februarie 2024
                         Vă mulțumim pentru alegerea de a utiliza serviciile hotelului nostru. Suntem dedicați
                         protejării confidențialității și securității informațiilor dumneavoastră personale. Această
                         politică descrie practicile noastre în colectarea, utilizarea și protejarea datelor personale
-                        în contextul activităților de ofertare, rezervare și alte servicii asociate hotelului.
+                        în contextul activităților de ofertare, rezervare și alte servicii asociate hotelului.<br></br>
                         Informații Colectate
                         Pentru a vă oferi serviciile noastre hoteliere, colectăm următoarele categorii de
                         informații:
                          Informații personale: nume, adresă, e-mail, număr de telefon și alte detalii de
-                        contact, necesare pentru crearea unui cont sau efectuarea unei rezervări.
+                        contact, necesare pentru crearea unui cont sau efectuarea unei rezervări.<br></br>
                          Detalii rezervare: informații despre preferințele dvs. de cazare, inclusiv datele
-                        rezervărilor, solicitările speciale și feedback-ul privind șederea la hotel.
+                        rezervărilor, solicitările speciale și feedback-ul privind șederea la hotel.<br></br>
                          Informații de utilizare: date despre cum interacționați cu site-ul nostru, precum
                         paginile accesate și serviciile utilizate, pentru a îmbunătăți experiența dvs. pe
-                        site.
+                        site.<br></br>
                         Utilizarea Informațiilor
                         Informațiile colectate sunt utilizate în următoarele scopuri:
                          Pentru a procesa și gestiona rezervările: confirmarea detaliilor rezervării,
-                        oferirea de asistență și comunicarea modificărilor.
+                        oferirea de asistență și comunicarea modificărilor.<br></br>
                          Pentru îmbunătățirea serviciilor: analiza utilizării site-ului și feedback-ul
-                        clienților pentru a îmbunătăți oferta și serviciile hoteliere.
+                        clienților pentru a îmbunătăți oferta și serviciile hoteliere.<br></br>
                          Comunicare: pentru a vă trimite informații relevante legate de rezervarea dvs. și
-                        pentru a oferi recomandări personalizate.
+                        pentru a oferi recomandări personalizate.<br></br>
                         Divulgarea Informațiilor
                         Informațiile personale nu vor fi dezvăluite terțelor părți fără consimțământul dvs. explicit,
                         cu excepția cazului în care este necesar pentru furnizarea serviciilor solicitate sau
-                        atunci când suntem obligați de lege.
+                        atunci când suntem obligați de lege.<br></br>
                         Securitatea Informațiilor
 
                         Adoptăm măsuri de securitate adecvate pentru a proteja informațiile personale
                         împotriva accesului neautorizat, utilizării necorespunzătoare sau divulgării. Totuși,
                         rețineți că nicio metodă de transmisie sau stocare electronică nu este complet sigură.
-                        Module Cookie și Tehnologii Similare
+                        Module Cookie și Tehnologii Similare<br></br>
                         Utilizăm module cookie pentru a îmbunătăți și personaliza experiența dvs. pe site-ul
                         hotelului. Aveți posibilitatea de a controla și de a refuza utilizarea modulelor cookie prin
-                        setările browserului dvs.
+                        setările browserului dvs.<br></br>
                         Drepturile Utilizatorilor
                         Aveți dreptul de a accesa, rectifica sau șterge datele personale pe care le deținem
                         despre dvs. De asemenea, puteți solicita restricționarea prelucrării sau să vă opuneți
-                        prelucrării datelor dvs. personale.
+                        prelucrării datelor dvs. personale.<br></br>
                         Modificări ale Politicii de Confidențialitate
                         Politica noastră de confidențialitate poate fi actualizată periodic. Orice modificări vor fi
                         comunicate prin actualizarea acestei pagini. Utilizarea continuă a serviciilor noastre
-                        după aceste modificări constituie acceptarea noilor termeni.
+                        după aceste modificări constituie acceptarea noilor termeni.<br></br>
                         Contact
                         Pentru întrebări sau solicitări legate de această politică, vă rugăm să ne contactați prin
                         e-mail la office@hotel-olimp.ro sau prin intermediul formularului de contact de pe site-ul
-                        nostru.
+                        nostru.<br></br>
                         Prin furnizarea datelor personale, recunoașteți și acceptați prelucrarea acestora în
                         conformitate cu această politică de confidențialitate și legislația aplicabilă</p>
                 </div>
