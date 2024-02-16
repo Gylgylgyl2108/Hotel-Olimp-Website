93e63600bc95af5a9ad7728832c90715999523e9
Gylgylgyl2108 <138683530+Gylgylgyl2108@users.noreply.github.com>
2024-02-16 19:23:22 +0200

    modificare pret

--- preturi.php
+++ preturi.php
@@ -4,7 +4,7 @@
     $camera_double_deluxe = 200;
     $camera_triple = 260;
     $camera_double_simple = 180;
-    $camera_twin = 170;
+    $camera_twin = 250;
     $camera_four_guests = 320;
 
     // Orarul pentru mic-dejun (inlocuiesti ce e in Ghilimele)
