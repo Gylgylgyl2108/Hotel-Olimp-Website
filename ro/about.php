dd9bacb4b261ebc81bd0a87e849550a266f8b327
Titi923 <virgil.cenan@gmail.com>
2024-02-17 09:41:42 +0200

    links

--- ro/about.php
+++ ro/about.php
@@ -60,17 +60,17 @@
                     <div class="global-slick-init attraction-slider slider-inner-margin" data-slidesToShow="6" data-infinite="true" data-arrows="false" data-dots="false" data-swipeToSlide="true" data-autoplay="true" data-autoplaySpeed="2500" data-prevArrow='<div class="prev-icon"><i class="las la-angle-left"></i></div>'
                         data-nextArrow='<div class="next-icon"><i class="las la-angle-right"></i></div>' data-responsive='[{"breakpoint": 1400,"settings": {"slidesToShow": 5}},{"breakpoint": 1200,"settings": {"slidesToShow": 4}},{"breakpoint": 992,"settings": {"slidesToShow": 3}},{"breakpoint": 576, "settings": {"slidesToShow": 2} }]'>
                         <div class="single-brand">
-                            <a href="javascript:void(0)" class="single-brand-thumb">
+                            <a href="https://www.booking.com/hotel/ro/olimp.ro.html" class="single-brand-thumb">
                                 <img src="assets/img/single-page/logo1.png" alt="brandLogo">
                             </a>
                         </div>
                         <div class="single-brand">
-                            <a href="javascript:void(0)" class="single-brand-thumb">
+                            <a href="https://www.tripadvisor.com/" class="single-brand-thumb">
                                 <img src="assets/img/single-page/logo2.png" alt="brandLogo">
                             </a>
                         </div>
                         <div class="single-brand">
-                            <a href="javascript:void(0)" class="single-brand-thumb">
+                            <a href="https://www.airbnb.com.ro/" class="single-brand-thumb">
                                 <img src="assets/img/single-page/logo3.png" alt="brandLogo">
                             </a>
                         </div>
