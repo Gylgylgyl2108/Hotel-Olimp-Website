10d6fbc401be0c2b31df0db92678f010eeb22235
Titi923 <virgil.cenan@gmail.com>
2024-02-17 09:57:33 +0200

    test

--- ro/contact_forms/process_contact.php
+++ ro/contact_forms/process_contact.php
@@ -1,6 +1,5 @@
 <?php
-// Set subject
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['recaptcha_response'])) {
+if (!empty($_POST['recaptcha_response'])) {
     // Build POST request:
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = '6LeS_3MpAAAAAFpMAGckDYSJbYFN3nR3nIvmJ4Fp';
@@ -9,6 +8,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['recaptcha_response'])
     // Make and decode POST request:
     $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
     $recaptcha = json_decode($recaptcha);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
     
     // Sanitize user input to prevent security issues
     $name = htmlspecialchars($_POST["name"]);
